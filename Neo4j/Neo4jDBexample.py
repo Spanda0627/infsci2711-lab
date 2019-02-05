ba8ac12bdb644e5cd15bc3fe4866c58f6a69439e
yfamy123 <yfamy123@gmail.com>
2019-02-05 16:16:38 -0500

    Add files via upload

--- Neo4j/Neo4jDBexample.py
+++ Neo4j/Neo4jDBexample.py
@@ -1,19 +1,22 @@
-from py2neo import Graph, Node, Relationship
+from py2neo import Graph, Node, Relationship, NodeMatcher, RelationshipMatcher
 
-def cleanDB():
+
+def clean_db(graph):
     graph.delete_all()
 
-def showAll():
-    results = graph.find(label="Person")
+
+def show_all(node_matcher):
+    results = node_matcher.match("Person")
     for f in results:
         print(f)
     print("========================")
 
-def addNodeRel():
-    peter=Node("Person", "student", name="Peter", age=25, gender="male")
-    kelly=Node("Person", "player", name="Kelly", age=20, gender="female")
-    kary=Node("Person", "student", name="Kary", age=25, gender="female")
-    alia=Node("Person", "officer", name="alia", age=23, gender="female")
+
+def add_node_rel(graph):
+    peter = Node("Person", "student", name="Peter", age=25, gender="male")
+    kelly = Node("Person", "player", name="Kelly", age=20, gender="female")
+    kary = Node("Person", "student", name="Kary", age=25, gender="female")
+    alia = Node("Person", "officer", name="alia", age=23, gender="female")
     graph.create(peter)
     graph.create(kelly)
     graph.create(kary)
@@ -23,31 +26,43 @@ def addNodeRel():
     aFriendRel['years'] = 2
     graph.create(aFriendRel)
 
-def update():
-    kary=Node("Person", "student", name="Kary", age=25, gender="female")
-    peter=Node("Person", "student", name="Peter", age=25, gender="male")
+
+def update(graph):
+    kary = Node("Person", "student", name="Kary", age=25, gender="female")
+    peter = Node("Person", "student", name="Peter", age=25, gender="male")
     aFriendRel = Relationship(kary, 'friends_of', peter)
     aFriendRel['years'] =2
     graph.push(aFriendRel)
 
-def search():
-    result = graph.find_one(
-        label="Person",
-        property_key="gender",
-        property_value="female"
-    )
+
+def search_node(node_matcher):
+    result = node_matcher.match("Person", gender="female").first()
     print(result)
-    print(result['name'])
 
 
-graph = Graph("localhost:7474", username = "neo4j", password = "112358")
-cleanDB()
-addNodeRel()
-showAll()
-update()
-showAll()
-search()
+def search_rel(rel_matcher):
+    kary = node_matcher.match("Person", name="Kary").first()
+    peter = node_matcher.match("Person", name="Peter").first()
+    result = rel_matcher.match((kary, peter)).first()
+    print(result)
 
+
+try:
+    graph = Graph("localhost:7474", username="neo4j", password="amy920420")
+    node_matcher = NodeMatcher(graph)
+    rel_matcher = RelationshipMatcher(graph)
+except:
+    print("An exception occurred")
+    raise
+
+clean_db(graph)
+add_node_rel(graph)
+show_all(node_matcher)
+print("\n********************\nUpdate the graph\n")
+update(graph)
+show_all(node_matcher)
+search_node(node_matcher)
+search_rel(rel_matcher)
 
 
 
